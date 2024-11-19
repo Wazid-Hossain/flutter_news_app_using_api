5d13538429cb0975dd154ea4662878e7a8bb53d1
wazid hossain <mwh.wajid96@gmail.com>
2024-11-20 02:14:28 +0600

    url's added

---
+++ lib/api_service.dart
@@ -0,0 +1,7 @@
+class ApiService {
+
+final all_news_url = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=987ff5b8314348a8bc12798bfedb23c7";
+final breaking_news_url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=987ff5b8314348a8bc12798bfedb23c7";
+
+  
+}
