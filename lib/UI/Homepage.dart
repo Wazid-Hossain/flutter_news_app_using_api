81f5a9951b63dfce2bb8b9dfe6af379993c881d7
wazid hossain <mwh.wajid96@gmail.com>
2024-11-22 00:03:36 +0600

    creating breaking and all news tab

---
+++ lib/UI/Homepage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:news_app_using_api/UI/all_news.dart';
+import 'package:news_app_using_api/UI/breaking_news.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController
+    (length: 2, child: Scaffold
+    (appBar: AppBar
+    (title: Text("Flutter News App"),
+    bottom: TabBar(tabs: [
+      Tab(text: "Braking News",
+            Tab(text: "All News",)
+
+      )
+    ]),
+    ),
+    body: TabBarView(children: [
+      BreakingNews();
+      AllNews();
+    ]),
+    )
+    );
+  }
+}
